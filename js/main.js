e8e934a38388d40639d7bd9c377a89bfc59993e9
National-student <sanjarshopulatov62@gmail.com>
2022-03-07 14:12:53 +0500

    initial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 // js file
+
+var elForm = document.querySelector(".form");
+var elInput = document.querySelector(".input");
+var elList = document.querySelector(".list");
+var newArray = [];
+
+
+elForm.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    newArray.push(elInput.value);
+    elList.innerHTML = null
+    
+    for (var toDo of newArray) {
+            var elItem = document.createElement("li");
+            elItem.textContent = toDo
+            elList.appendChild(elItem);
+        }
+        elInput.value = null
+})
+
+
